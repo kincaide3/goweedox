d37143eed3b9741c4f666a9200ef725033d2a354
el <el.parac@gmail.com>
2010-05-14 11:44:00 +0800

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users
+  map.root :controller => "welcome"
 
   # The priority is based upon order of creation: first created -> highest priority.
 
